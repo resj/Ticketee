49f527fbd60354cac08d2d8c28bd4f2c7d88fe6c
resj <raymons17@gmail.com>
2015-05-21 22:49:31 -0700

    Implemented listin tickets on project show view

---
+++ spec/factories/ticket_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+	factory :ticket do
+		title "Example ticket"
+		description "just an boring empty ticket"
+	end
+end
---
+++ spec/features/viewing_tickets_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+feature "Viewing tickets" do
+	before do
+		textmate_2 = FactoryGirl.create(:project, name: "Textmate 2")
+
+		FactoryGirl.create(:ticket, project: textmate_2, title: "make it rain",
+			description: "on them bitches or hoes!")
+
+		internet_explorer = FactoryGirl.create(:project, name: "Internet Explorer")
+
+		FactoryGirl.create(:ticket, project: internet_explorer, title: "You cray cray",
+			description: "ie is for fools nigga")
+
+		visit '/'
+	end
+
+	scenario "Viewing tickets for a given page" do
+		click_link "Textmate 2"
+
+		expect(page).to have_content("make it rain")
+		expect(page).to_not have_content("You cray cray")
+
+		click_link "make it rain"
+		within("#ticket h2") do
+			expect(page).to have_content("make it rain")
+		end
+
+		expect(page).to have_content("on them bitches or hoes!")
+	end
+end
+
