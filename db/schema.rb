f18f7fdc0904bdd77d7dae7d79c3865c125323de
resj <raymons17@gmail.com>
2015-05-24 16:22:29 -0700

    Make a User model with password.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521015802) do
+ActiveRecord::Schema.define(version: 20150524222222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20150521015802) do
   end
 
   add_index "tickets", ["project_id"], name: "index_tickets_on_project_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   add_foreign_key "tickets", "projects"
 end
