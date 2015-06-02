504fd879d906d6cc4b406f76bdacca98379fbe54
resj <raymons17@gmail.com>
2015-06-01 20:41:27 -0700

    Added the admin field to user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
       redirect_to root_url
     else
       flash[:error] = "Sorry."
-      render new
+      render "new"
     end
   end
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name "Example user"
-    email "user@example.com"
+    name "username"
+    email "sample@example.com"
     password "hunter2"
     password_confirmation "hunter2"
+
+    factory :admin_user do 
+      admin true
+    end
   end
 end
